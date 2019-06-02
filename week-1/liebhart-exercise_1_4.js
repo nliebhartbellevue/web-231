d5cef2b26f75eacca119d3ed51362ae1dc868538
Nathaniel D. Liebhart <nliebhart@my365.bellevue.edu>
2019-06-02 15:32:04 -0500

    Exercise 1.4 commit

---
+++ week-1/liebhart-exercise_1_4.js
@@ -0,0 +1,27 @@
+/*
+  Title: Exercise 1.4
+  Author: Nathaniel D. Liebhart
+  Date: June 2, 2019
+  Description: Program to demonstrate the use of primitive types.
+*/
+
+// Variable declaration
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var zipCode;
+
+// Variable assignment
+firstName = 'Nathaniel';
+lastName = 'Liebhart';
+age = 31;
+street = 'Lincoln Ave';
+city = 'Roseland';
+zipCode = '68973';
+
+// Output
+console.log(
+	`${firstName} ${lastName} is ${age} years old. \nAnd lives in ${city},${zipCode} on ${street}`
+);
