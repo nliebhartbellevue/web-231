e7eb161bed225328a1eabb717fae349795b89cd6
Nathaniel D. Liebhart <nliebhart@my365.bellevue.edu>
2019-06-02 15:20:16 -0500

    Added code attributions

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,8 @@
+/*
+  Title: Exercise 1.3 - Hello World
+  Author: Nathaniel D. Liebhart
+  Date: June 2, 2019
+  Description: Simple console log to demonstrate a Hello World program.
+*/
+
 console.log('Hello World, my name is Nathaniel D. Liebhart');
