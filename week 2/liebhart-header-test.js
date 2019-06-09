b2a2081f7b9e0dad8c14c51ecd3894ac0477300c
Nathaniel D. Liebhart <nliebhart@my365.bellevue.edu>
2019-06-09 17:07:21 -0500

    Week 2 - Exercise 2.2 commit

---
+++ week 2/liebhart-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Nathaniel D. Liebhart 
+; Date:   June 9th 2019
+; Modified By: 
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// import the header.js file from the root dir
+const header = require('../liebhart-header');
+
+console.log(header.display('Nathaniel', 'Liebhart', 'Exercise 2.2'));
